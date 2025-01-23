57b3159712c3db56154bcaa270957100cca52a8a
CHARAN. B <charanbalaji03@gmail.com>
2025-01-23 18:59:26 +0530

    Update Practice 1. Palindrome Checker.py

--- Practice 1. Palindrome Checker.py
+++ Practice 1. Palindrome Checker.py
@@ -1,16 +1,16 @@
 #verify whether the given word is a palindrome or not.
 
-def is_palindrome(s):                        #here i'm defining a function to check whether word is palindrome.
-    start, end = 0, len(s) - 1               #defining the necessary variables.
+def is_palindrome(s):                        
+    start, end = 0, len(s) - 1               
     while start < end:                       #using while statement to check whether start value is less that end value.
         if s[start] != s[end]:               #using if statement, we check the inequality between start and end of the word.
-            return False                     #returning false id the don't match.
-        start += 1                           #Moving the start pointer towards the right .
-        end -= 1                             #moving the end pointer towards the left.
-    return True                              #If the loop completes without returning False, the string is a palindrome.
+            return False                     
+        start += 1                          
+        end -= 1                       
+    return True                              
 
-word = input("Enter a word: ")               #in this line, we are getting input form the user.
+word = input("Enter a word: ")             
 if is_palindrome(word):                      #here, we use if statement, that calls the function & verify the condition.
-    print(f"{word} is a palindrome!")        #this print statement will executes when the word is actually palindrome.
+    print("It is a palindrome!")      
 else:                                        #if the condition does not satisfies, else part will be executing.
-    print(f"{word} is not a palindrome.")    #this print statement will executes when the word is not a palindrome.
+    print("It is not a palindrome.")    
