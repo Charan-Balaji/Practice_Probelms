9b4b88e45766603024c1d8e5a2eb7f4351be53fa
CHARAN. B <charanbalaji03@gmail.com>
2025-01-23 19:56:03 +0530

    Update practice6_count_vowels.py

--- practice6_count_vowels.py
+++ practice6_count_vowels.py
@@ -1,12 +1,12 @@
 #program to count the number of vowels ina string
 
-def count_vowels(s):                # Function to count vowels in a string
-    vowels = 'aeiou'                # Vowel characters
-    count = 0                       # Initialize the counter for vowels
+def count_vowels(s):                
+    vowels = 'aeiou'                
+    count = 0                     
     for char in s:                  # Loop through each character in the string
         if char.lower() in vowels:  # If the character is a vowel
             count += 1              # Increment the counter
     return count
 
-string = input("Enter a string: ")  # Taking user input for a string
-print(f"The number of vowels in the string is {count_vowels(string)}.")  # Output the count of vowels
+word = input("Enter a string: ") 
+print("The number of vowels in the string is", count_vowels(word))  # Output the count of vowels
