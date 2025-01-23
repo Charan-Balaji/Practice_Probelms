0abf0cf6e5c64393c3fb83569da64857b42b203a
CHARAN. B <charanbalaji03@gmail.com>
2025-01-23 19:03:50 +0530

    Update practice2_fibonnaci_series.py

--- practice2_fibonnaci_series.py
+++ practice2_fibonnaci_series.py
@@ -9,5 +9,5 @@ def fibonacci(n):
         a, b = b, a + b                       # Update the values of 'a' and 'b' for the next term
 
 n = int(input("Enter the number of terms: ")) # Take input from the user for the number of terms to generate
-print("Fibonacci Series for term- "+n)    
+print("Fibonacci Series for term - ",+n)    
 fibonacci(n)                                  # Call the fibonacci function to print the sequence
