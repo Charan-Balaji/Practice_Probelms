10d14fc4553a23147842b62b2c5809eb2aa005b1
CHARAN. B <charanbalaji03@gmail.com>
2025-01-23 19:36:59 +0530

    Update practice4_factorial.py

--- practice4_factorial.py
+++ practice4_factorial.py
@@ -1,9 +1,9 @@
 #find the factorial of 'n' number.
 
-def factorial(n):  # Function to calculate the factorial using recursion
-    if n == 0 or n == 1:  # Base case: if n is 0 or 1, return 1
+def factorial(n):  
+    if n == 0 or n == 1:  #if n is 0 or 1, we are returning 1
         return 1
-    return n * factorial(n - 1)  # Recursive case: n multiplied by factorial of (n-1)
+    return n * factorial(n - 1)  # n multiplied by factorial of (n-1)
 
-number = int(input("Enter a number: "))  # Take input from the user
-print(f"The factorial of {number} is {factorial(number)}.")  # Print the result of the factorial calculation
+number = int(input("Enter a number: "))  
+print("The factorial of", number, "is", factorial(number))   # Print the result of the factorial
