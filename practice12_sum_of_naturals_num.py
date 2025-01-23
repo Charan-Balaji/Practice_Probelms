72c39b5c436a10e9a8f9291731bb6f8287db8ea0
CHARAN. B <charanbalaji03@gmail.com>
2025-01-23 19:01:22 +0530

    Delete practice12_sum_of_naturals_num.py

--- practice12_sum_of_naturals_num.py
+++
@@ -1,6 +0,0 @@
-#find the sum of 'n' natural numbers
-def sum_of_natural_numbers(n): # Function to calculate the sum of natural numbers up to 'n'
-    return n * (n + 1) // 2    # Formula to calculate the sum of natural numbers
-
-number = int(input("Enter a number: "))  # Taking user input for the number
-print(f"The sum of the first {number} natural numbers is: {sum_of_natural_numbers(number)}.")  # Output the sum
