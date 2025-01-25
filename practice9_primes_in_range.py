bae377394379cd652ed592baf229587405d08ef6
CHARAN. B <charanbalaji03@gmail.com>
2025-01-25 20:06:12 +0530

    Update practice9_primes_in_range.py

--- practice9_primes_in_range.py
+++ practice9_primes_in_range.py
@@ -1,15 +1,15 @@
 #find the prime numbers in a given range 
-def find_primes(start, end):                         # Function to find all prime numbers in a given range
+def find_primes(start, end):                      
     primes = []                                      # List to store prime numbers
-    for num in range(start, end + 1):                # Loop through the range from 'start' to 'end'
+    for num in range(start, end):                
         if num > 1:                                  # A prime number is greater than 1
             for i in range(2, int(num ** 0.5) + 1):  # Check divisibility from 2 to square root of num
                 if num % i == 0:                     # If num is divisible by any number, it's not prime
-                    break                            #break to exit the loop
-            else:                                    # If no divisor found, the number is prime
-                primes.append(num)                   # call the function
+                    break                            
+            else:                                   
+                primes.append(num)                  
     return primes                                    # Return the list of prime numbers
 
-start = int(input("Enter the start of the range: ")) # Taking user input for start
-end = int(input("Enter the end of the range: "))     # Taking user input for end
-print(f"Prime numbers between {start} and {end}: {find_primes(start, end)}.")  # Output the list of primes
+start = int(input("Enter the start of the range: ")) # Taking user input 
+end = int(input("Enter the end of the range: "))    
+print("Prime numbers between the given range is : " find_primes(start, end))  # Output the list of primes
