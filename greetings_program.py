56804efe3aef42b8a37c21b9e0efdf0e9e5faddc
CHARAN. B <charanbalaji03@gmail.com>
2025-01-29 19:21:40 +0530

    Add files via upload

---
+++ greetings_program.py
@@ -0,0 +1,3 @@
+# Write a program to check the given number is divisible by both 3 and 4 if it is so print "Good morning". 
+# If the number is divisible by only 4, but not by 3 then print "Good afternoon". if it is divisible by only 3 and not by 4, then "Good evening". otherwise, "Good night".
+
